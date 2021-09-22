8224eaf5a66e73f4a22c12b84299787aff3cd772
Kevin <kevinalexanderduran8@gmail.com>
2021-09-21 17:42:57 -0700

    ...testing

--- todo/src/Nav.js
+++ todo/src/Nav.js
@@ -5,12 +5,16 @@ import logo from './assets/cyborg.jpeg'
 const Nav = () => {
     return (
        <nav>
-           <div><img className="logoImage" src={logo} alt="logo cyborg"></img></div>
-           <ul>
-               <li>kill all humans</li>
-               <li>make cure for cancer</li>
-               <li>start WW3</li>
-           </ul>
+           <div className="logoContainer"><img className="logoImage" src={logo} alt="logo cyborg"></img></div>
+           <div className="linkContainer">
+            <ul>
+                <li>kill all humans</li>
+                <li>make cure for cancer</li>
+                <li>start WW3</li>
+            </ul>
+           </div> 
+           <div className="hamburger"></div>
+           <div className="overlayMenu"></div>
        </nav>
     )
 }
