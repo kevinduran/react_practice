919652c98c056cff8676f0e3a3152941f57f6790
Kevin <kevinalexanderduran8@gmail.com>
2021-09-27 06:41:36 -0700

    testing

---
+++ testing/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+    
+
+
+
+    return (
+        <div>
+            <div className = "output">0</div>
+            <div className = "btnContainer">
+                <button>+</button>
+            </div>
+        </div>
+    )
+}
