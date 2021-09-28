45c689be42f816eeec6a8f39645311ddc9513933
Kevin <kevinalexanderduran8@gmail.com>
2021-09-28 08:33:07 -0700

    testing

--- testing/src/App.js
+++ testing/src/App.js
@@ -2,16 +2,21 @@ import React from 'react'
 import './App.css'
 
 export default function App() {
+
+   
     
-
-
+    const fetchMyData = ()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(res=>res.json())
+            .then(response=>{
+                console.log(response);
+            });    
+    }   
 
     return (
         <div>
-            <div className = "output">0</div>
-            <div className = "btnContainer">
-                <button>+</button>
-            </div>
+            <div className="output">hello there</div>
+            <button onClick={fetchMyData}>get data</button>
         </div>
     )
 }
