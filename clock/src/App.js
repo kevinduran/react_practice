ed63d2aa5ab3077d2d3acbe2b3890d52622bebb6
Kevin <kevinalexanderduran8@gmail.com>
2021-10-02 05:52:44 -0700

    ...testing

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,17 +1,26 @@
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [time,setTime] = useState('')
+
+  setInterval(()=>{
+    setTime(new Date().toLocaleTimeString())
+  },1000)
+
   return (
     <div>
       <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap');
       </style>
       <div className="time">
-
-        {new Date().toLocaleTimeString()}
+        {time}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
