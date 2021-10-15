72ef3d835db940386eaf5eb7a1fe0220d7f322fa
Kevin <kevinalexanderduran8@gmail.com>
2021-10-15 10:48:28 -0700

    ...testing

--- testing/src/Person.js
+++ testing/src/Person.js
@@ -1,12 +1,26 @@
 import React, {useState} from 'react'
 
 export default function Person() {
-    const [person,setPerson] = useState('')
+    const [person, setPerson] = useState('')
+    const[name, setName] = useState('')
+    const makeFriend = () => {
+        fetch('https://randomuser.me/api/')
+        .then(res=> res.json())
+        .then(data=>{
+            setPerson(data.results[0].picture.thumbnail)
+            setName(data.results[0].name.first)
+        })
+        
+    }
 
     return (
         <div className="bigBoy">
-            <button className="friendBtn btn">friend</button>
-            hello, {person}.
+            <button 
+                className="friendBtn btn"
+                onClick={makeFriend}
+            >hello</button>
+            hello, {name}
+            <img alt="" src={person}/>.
         </div>
     )
 }
