6d44bcace92b4785afad4efba228d2a7b161eb45
Kevin <kevinalexanderduran8@gmail.com>
2021-10-25 19:58:01 -0700

    tesing...

--- testing/src/Dark.js
+++ testing/src/Dark.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import logo from './hell.gif'
 
 export default function Darks() {
     return (
         <div className="bigBoy">
-            hello friend
+        <div className = "hell">
+            <img src={logo} alt="fail"/>  
+        </div>
+        <div className="sliderToggleContainer"> 
+           <h4>turn on</h4>
+           <label className="switch">
+            <input type="checkbox"/>
+            <span className='slider round'></span>
+           </label>
+           <h4 className="red">hell</h4>
+        </div>
+
         </div>
     )
 }
