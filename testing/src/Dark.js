988d4fce89dd4c3153ce376d3304b8ec7ac97fa4
Kevin <kevinalexanderduran8@gmail.com>
2021-10-25 20:47:27 -0700

    testing...

--- testing/src/Dark.js
+++ testing/src/Dark.js
@@ -2,19 +2,23 @@ import React from 'react'
 import logo from './hell.gif'
 
 export default function Darks() {
+
+    const addHell = () =>{
+     document.querySelector('.hellYes').classList.remove('hellNo')
+    }
+
     return (
         <div className="bigBoy">
-        <div className = "hell">
-            <img src={logo} alt="fail"/>  
-        </div>
-        <div className="sliderToggleContainer"> 
-           <h4>turn on</h4>
-           <label className="switch">
-            <input type="checkbox"/>
-            <span className='slider round'></span>
-           </label>
-           <h4 className="red">hell</h4>
-        </div>
+            <div className="sliderToggleContainer"> 
+            <h4 className="red">hell</h4>
+            <label onClick={addHell} className="switch">
+                <input type="checkbox"/>
+                <span className='slider round'></span>
+            </label>
+            </div>
+            <div className = "hell">
+                <img className="hellYes hellNo" src={logo} alt="fail"/>  
+            </div>
 
         </div>
     )
