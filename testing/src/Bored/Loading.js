03f2aafbd048bb2ffd86dbe00fd7a8519380120e
Kevin <kevinalexanderduran8@gmail.com>
2021-11-20 07:31:24 -0800

    test

--- testing/src/Bored/Loading.js
+++ testing/src/Bored/Loading.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <div>
-           <h5> I have an idea, why don't you...</h5>
+        <div className="loader">
+          
         </div>
     )
 }
