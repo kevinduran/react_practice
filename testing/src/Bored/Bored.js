e0677f6959a7074dd742f5d4ad235ed2813dacdb
Kevin <kevinalexanderduran8@gmail.com>
2021-11-20 05:48:53 -0800

    test

---
+++ testing/src/Bored/Bored.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Loading from './Loading'
+export default function Bored() {
+    const [output, setOutput] = useState('')
+    const [loading, setLoading] = useState(true)
+
+
+    const fun= () => {
+        setLoading(false)
+        fetch('https://www.boredapi.com/api/activity')
+        .then(res=>res.json())
+        .then(data=> {
+            setOutput(data.activity)
+            setLoading(true)
+        })
+    }
+
+    
+if(loading){
+    return (
+        <div class="bigBoy">
+            <button className="btn btn-top" onClick={fun}>i'm bored</button>
+            <div className="boredOutput">
+                <h5>{output}</h5>        
+            </div>
+        </div>
+    )
+}else{
+    return (
+        <div class="bigBoy">
+            <button className="btn btn-top" onClick={fun}>i'm bored</button>
+            <div className="boredOutput">
+                <Loading />          
+            </div>
+        </div>
+    )
+}
+    
+}
