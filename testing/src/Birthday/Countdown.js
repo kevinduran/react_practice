5ceb57ff6966ed18675376e567c7e624b0ce5f80
Kevin <kevinalexanderduran8@gmail.com>
2021-11-25 03:00:58 -0800

    tes

--- testing/src/Birthday/Countdown.js
+++ testing/src/Birthday/Countdown.js
@@ -3,12 +3,19 @@ import moment from 'moment';
 
 
 
-export default function Countdown(birthday) {
+export default function Countdown(birthdayData) {
     var today = moment(new Date().toLocaleDateString());
-    var birthdayNext = moment(birthday.birthday);
+    var nextYear = new Date().getFullYear()+1;
+    
+    var birthday = birthdayData.birthday
+
+    var birthdayNextDate = nextYear + birthday.slice(4)
+    
+    var birthdayNext = moment(birthdayNextDate);
     return (
         <div>
-             <p className="redTiny">birthday is {today.from(birthdayNext)}</p>
+             <p className="redTiny">birthday is {birthdayNext.from(today)}</p>
         </div>
     )
 }
+
