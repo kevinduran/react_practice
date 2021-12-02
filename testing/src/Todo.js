b29a83956b3a5be2e1dfe54a86462f52b5a74eaa
Kevin <kevinalexanderduran8@gmail.com>
2021-12-02 11:05:50 -0800

    CHECKPOINT

--- testing/src/Todo.js
+++ testing/src/Todo.js
@@ -1,36 +1,38 @@
 import React,{useState} from 'react'
-
 export default function Todo() {
     const [todo,setTodo] = useState([]);
 
-    const remove = () => {
-        console.log('removed')
-       
-    }
-
+    
     const addTodo = () => {
-        setTodo([...todo,
-            <div className="todoItem">
-                {document.querySelector('.todoInput').value}
-                <button className="btn" onClick={remove}>x</button>
-            </div>
-        ])
+        var me = document.querySelector('.todoInput').value
+        setTodo([...todo,{name: me }])
         document.querySelector('.todoInput').value = '';
     }
-
+    const remove = (name) => {
+        console.log(todo)
+       const removed = todo.filter((key)=>{
+           return (key.name !== name)
+       })
+       setTodo(removed)
+    }
 
     return (
         <div className="bigBoy">
             <div className="todoContainer">
-                <input
-                    className="todoInput"            
-                />
-                <button onClick={addTodo} className="btn">+</button>
+                <input className="todoInput" />
+                <button onClick={()=>addTodo()} className="btn">+</button>
             </div>
 
-            <div className="todoItemContainer">{todo}</div>
+            <div className="todoItemContainer">
+                {todo.map((myTodo)=>{
+                    return (<div key={todo.length+1} className="todoItem">
+                                <p>{myTodo.name}</p>
+                                <button className='btn' onClick={()=>remove(myTodo.name)}>x</button>  
+                            </div>)
+                        
+                })}
+            </div>
         </div>
     )
 }
-
 
